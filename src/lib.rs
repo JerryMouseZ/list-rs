9c634563e991a9a0612102edbed9b36a6f48f4c5
JerryMouseZ <1711333@mail.nankai.edu.cn>
2024-07-23 23:16:54 +0800

    init a cell list

--- src/lib.rs
+++ src/lib.rs
@@ -1,100 +1,115 @@
-use std::fmt;
-// write a list in Rust
+use std::cell::RefCell;
+use std::rc::Rc;
+
 #[allow(dead_code)]
-pub enum List<T> {
-    Cons(T, Box<List<T>>),
-    Nil,
+type Link<T> = Option<Rc<RefCell<Node<T>>>>;
+
+#[derive(Debug)]
+pub struct Node<T> {
+    pub elem: T,
+    next: Link<T>,
 }
 
-impl<T: PartialEq + fmt::Debug> List<T> {
-    pub fn new() -> List<T> {
-        List::Nil
+impl<T> Node<T> {
+    fn new(x: T) -> Self {
+        Node::<T> {
+            elem: x,
+            next: None,
+        }
+    }
+}
+
+pub struct List<T> {
+    head: Link<T>,
+    tail: Link<T>,
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List::<T> {
+            head: None,
+            tail: None,
+        }
     }
 
-    pub fn insert(self, value: T) -> Self {
-        List::Cons(value, Box::new(self))
+    pub fn push_front(&mut self, x: T) {
+        // 生成新节点
+        let new_node = Rc::new(RefCell::new(Node::<T>::new(x)));
+        // 所有权: list.old_head -> new_node.next
+        match self.head.take() {
+            // 如果头部为空，头和尾都变成新节点
+            None => {
+                self.tail = Some(new_node.clone());
+            }
+            Some(old_head) => {
+                (*new_node).borrow_mut().next = Some(old_head);
+            }
+        }
+        self.head = Some(new_node);
     }
 
-    pub fn delete(self, value: &T) -> Self {
-        match self {
-            List::Cons(val, next) => {
-                if val == *value {
-                    *next
-                } else {
-                    List::Cons(val, Box::new(next.delete(value)))
+    pub fn pop_front(&mut self) -> Link<T> {
+        // 删除头节点
+        // 1. 头节点为空，返回None
+        // 2. 头节点为最后一个元素，头和尾都置空
+        // 所有权 释放list.head, list.tail
+        // 3. 头节点不为最后一个元素
+        // 所有权old_head.next -> list.head, 释放list.old_head
+        match self.head.take() {
+            None => {
+                return None;
+            }
+            Some(head) => {
+                match (*head).borrow_mut().next.take() {
+                    None => {
+                        self.tail = None;
+                        self.head = None;
+                    }
+                    Some(next) => {
+                        self.head = Some(next);
+                    }
                 }
-            },
-            List::Nil => {
-                List::Nil
+                Some(head)
             }
         }
     }
 
-    pub fn find(&self, value: &T) -> &Self {
-        let mut node = self;
-        while let List::Cons(val, next) = node {
-            if *val == *value {
-                return &node;
+    pub fn push_back(&mut self, x: T) {
+        let new_node = Rc::new(RefCell::new(Node::new(x)));
+        match self.tail.take() {
+            None => {
+                self.head = Some(new_node.clone());
             }
-            node = next;
+            Some(old_tail) => {
+                (*old_tail).borrow_mut().next = Some(new_node.clone());
+            }
         }
-        &List::Nil
-    }
-
-    pub fn print(&self) {
-        let mut node = self;
-        while let List::Cons(val, next) = node {
-            print!("{:?}, ", val);
-            node = next;
-        }
-        println!();
+        self.tail = Some(new_node);
     }
 }
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_new() {
-        let list: List<i32> = List::new();
-        match list {
-            List::Nil => assert!(true),
-            _ => assert!(false, "New list should be Nil"),
-        }
+    fn test_pop_front() {
+        let mut list = List::new();
+        list.push_front(1);
+        list.push_front(2);
+        list.push_front(3);
+        assert_eq!(list.pop_front().map(|node| (*node).borrow().elem), Some(3));
+        assert_eq!(list.pop_front().map(|node| (*node).borrow().elem), Some(2));
+        assert_eq!(list.pop_front().map(|node| (*node).borrow().elem), Some(1));
+
+        assert_eq!(list.pop_front().is_none(), true);
     }
 
     #[test]
-    fn test_insert() {
+    fn test_push_back() {
         let mut list = List::new();
-        list = list.insert(1);
-        match list {
-            List::Cons(val, _) => assert_eq!(val, 1),
-            _ => assert!(false, "List should not be Nil after insertion"),
-        }
-    }
-
-    #[test]
-    fn test_delete() {
-        let mut list = List::new();
-        list = list.insert(1);
-        list = list.delete(&1);
-        match list {
-            List::Nil => assert!(true),
-            _ => assert!(false, "List should be Nil after deletion"),
-        }
-    }
-
-    #[test]
-    fn test_find() {
-        let mut list = List::new();
-        list = list.insert(1);
-        let found = list.find(&1);
-        match found {
-            List::Cons(val, _) => assert_eq!(*val, 1),
-            _ => assert!(false, "Should find the inserted value"),
-        }
+        list.push_back(1);
+        list.push_back(2);
+        list.push_back(3);
     }
 }
-
--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,9 @@ use list_rs::List;
 
 fn main() {
     let mut list = List::new();
-    for i in 0..100 {
-        list = list.insert(i);
-    }
-    list.print();
-    list = list.delete(&50);
-
-    match list.find(&50) {
-        List::Cons(val, _) => println!("Found 50: {:?}", val),
-        List::Nil => println!("50 not found"),
-    }
-
-    match list.find(&49) {
-        List::Cons(val, _) => println!("Found 49: {:?}", val),
-        List::Nil => println!("49 not found"),
+    list.push_front(1);
+    list.push_front(2);
+    while let Some(node) = list.pop_front() {
+        println!("{}", (*node).borrow().elem);
     }
 }
